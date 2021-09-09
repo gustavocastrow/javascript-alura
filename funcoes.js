ab78a4753439672dc1e20f3f7bc6e9fb2401f8af
Gustavo Castro <gustavocastrocs@gmail.com>
2021-09-09 07:37:17 -0300

    Fundamentos Javascript

---
+++ funcoes.js
@@ -0,0 +1,40 @@
+// let x = "";
+// console.log(x);
+// x = "oi";
+
+// DECLARAÇÃO DE FUNÇÃO
+
+// 1) declara a função
+                      //2 -> valor de soma()
+function imprimeTexto(texto) {
+  console.log(texto)
+}
+
+// 2) executa a função (1 ou + vezes)
+
+imprimeTexto(soma());
+// imprimeTexto("outro texto");
+
+// três formas de escrever funções
+
+function soma(){
+  //outros códigos
+  //vários console.log()
+  return 2 + 2;
+}
+
+// console.log(soma())
+
+
+let listaFuncionarios = ['José', 'Ana', 'Luiza']
+let listaIdades = [22,19,33]
+
+const exibeLista = (lista, descricao) => {
+    e = ''
+        for (let i = 0; i < lista.length; i++){
+            e += '\n'+ descricao + lista [i]
+        }
+}
+
+console.log(exibeLista(listaFuncionarios, 'Funcionário: '))
+console.log(exibeLista(listaIdades, "Idades: "))
